afa2ec366baebe348d0c65c68e38ccd96f589439
waterwa1ker <ibragimov_kep@mail.ru>
2025-06-23 20:32:54 +0300

    movies done;

--- src/movielens_analysis.py
+++ src/movielens_analysis.py
@@ -1,12 +1,16 @@
 from sys import argv
 from links import Links
+from movies import Movies
 import json
 
 if __name__ == '__main__':
     if len(argv) == 5:
         #print(Links(argv[1]).top_directors(2))
-        print(Links(argv[1]).longest(2))
-        print(Links(argv[1]).top_cost_per_minute(3))
+        #print(Links(argv[1]).longest(2))
+        #print(Links(argv[1]).top_cost_per_minute(3))
         #dic = Links(argv[1]).most_expensive(4)
         #dic = Links(argv[1]).get_imdb(['0114709', '0113497', '0114709'], ['budget'])
         #print(Links(argv[1]).most_profitable(2))
+        #print(Movies(argv[2]).dist_by_release())
+        #print(Movies(argv[2]).dist_by_genres())
+        #print(Movies(argv[2]).most_genres(2))
--- src/movies.py
+++ src/movies.py
@@ -5,4 +5,60 @@ class Movies(Data):
     
     def __init__(self, file_path):
         super().__init__(file_path)
-        self.convert_types(get_class_name(self))   
+        self.convert_types(get_class_name(self))
+
+    def dist_by_release(self):
+        """
+        The method returns a dict or an OrderedDict where the keys are years and the values are counts.
+        You need to extract years from the titles. Sort it by counts descendingly.
+        """
+
+        years_dict = {}
+
+        movie_titles = self.__get_movie_titles()
+        years = []
+        for movie_title in movie_titles:
+            year = movie_title[movie_title.find('(')+1:movie_title.find(')')]
+            years.append(year)
+        for year in years:
+            if year not in years_dict:
+                years_dict[year] = 1
+            else:
+                years_dict[year] += 1
+
+        return dict(sorted(years_dict.items(), key=lambda x: x[1], reverse=True))
+
+    def dist_by_genres(self):
+        """
+        The method returns a dict where the keys are genres and the values are counts.
+     Sort it by counts descendingly.
+        """
+
+        genres_dict = {}
+
+        movies_genres = '|'.join(self.__get_movie_genres()).split('|')
+        for movie_genre in movies_genres:
+            if movie_genre not in genres_dict:
+                genres_dict[movie_genre] = 1
+            else:
+                genres_dict[movie_genre] +=1
+        return dict(sorted(genres_dict.items(), key=lambda x: x[1], reverse=True))
+
+    def most_genres(self, n):
+        """
+        The method returns a dict with top-n movies where the keys are movie titles and
+        the values are the number of genres of the movie. Sort it by numbers descendingly.
+        """
+
+        movies = {}
+        for movie in self.data:
+            movie_name = movie[1]
+            movie_genres = movie[2].split('|')
+            movies[movie_name] = len(movie_genres)
+        return dict(sorted(movies.items(), key=lambda x: x[1], reverse=True)[:n])
+
+    def __get_movie_titles(self):
+        return list(map(lambda x: x[1], self.data))
+
+    def __get_movie_genres(self):
+        return list(map(lambda x: x[2], self.data))
